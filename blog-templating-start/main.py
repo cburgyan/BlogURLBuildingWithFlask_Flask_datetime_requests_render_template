cdce63ac89548e5185a51913cbfab8d49e123a2d
cburgyan <cburgyan@gmail.com>
2022-07-17 09:49:42 -0500

    add Post class as project required

--- blog-templating-start/main.py
+++ blog-templating-start/main.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template
 import requests
+from post import Post
 
 
 app = Flask(__name__)
 
 url = 'https://api.npoint.io/ec84679d2403beee8160'
 blog_posts_json = requests.get(url=url).json()
+post_list = []
+for post in blog_posts_json:
+    post_list.append(Post(id=post["id"], title=post["title"], subtitle=post["subtitle"], body=post["body"]))
+
 
 @app.route('/')
 def home():
@@ -17,12 +22,11 @@ def post_page(blog_id):
     title = ""
     subtitle = ""
     body = ""
-    for post in blog_posts_json:
-
-        if post['id'] == int(blog_id):
-            title = post['title']
-            subtitle = post['subtitle']
-            body = post['body']
+    for post in post_list:
+        if post.id == int(blog_id):
+            title = post.title
+            subtitle = post.subtitle
+            body = post.body
             break
     return render_template('post.html', title=title, subtitle=subtitle, body=body)
 
