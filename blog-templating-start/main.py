40569ced9c221f584f65e005ec9cdbb8d68ce89f
cburgyan <cburgyan@gmail.com>
2022-07-17 09:34:09 -0500

    use jinja to connect and create urls

--- blog-templating-start/main.py
+++ blog-templating-start/main.py
@@ -1,11 +1,30 @@
 from flask import Flask, render_template
+import requests
 
 
 app = Flask(__name__)
 
+url = 'https://api.npoint.io/ec84679d2403beee8160'
+blog_posts_json = requests.get(url=url).json()
+
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template("index.html", posts=blog_posts_json)
+
+
+@app.route('/post/<blog_id>')
+def post_page(blog_id):
+    title = ""
+    subtitle = ""
+    body = ""
+    for post in blog_posts_json:
+
+        if post['id'] == int(blog_id):
+            title = post['title']
+            subtitle = post['subtitle']
+            body = post['body']
+            break
+    return render_template('post.html', title=title, subtitle=subtitle, body=body)
 
 if __name__ == "__main__":
     app.run(debug=True)
